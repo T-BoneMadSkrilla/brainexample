a2e93f06a1b0b0b9f6a0a2d50febf1d376a08889
T-BoneMadSkrilla <tylereftink@gmail.com>
2018-11-29 15:24:55 -0600

    got as much done in one day as possible

--- scripts.js
+++ scripts.js
@@ -1,39 +1,40 @@
 let trainedNet;
 
-function encode(arg){
-    return arg.split('').map(x => (x.charCodeAt(0) / 255));
+function encode(arg) {
+ return arg.split("").map(x => x.charCodeAt(0) / 256);
 }
 
 function processTrainingData(data) {
-    return data.map(d => {
-        return {
-            input: encode(d.input),
-            output: d.output
-        }
-    })
+ return data.map(d => {
+   return {
+     input: encode(d.input),
+     output: d.output
+   };
+ });
 }
 
-function train(data){
-    let net = new brain.NeuralNetwork();
-    net.train(processTrainingData(data));
-    trainedNet = net.toFunction();
-    alert('Finished training');
-};
-
-train(trainingData)
-
-function execute(input){
-    let results = trainedNet(encode(input));
-    let output;
-    results.trump > results.kardashian ? output = "Trump" : output = "Kardashian";
-    return output;
+function train(data) {
+ let net = new brain.NeuralNetwork();
+ net.train(processTrainingData(data));
+ trainedNet = net.toFunction();
 }
 
-// function myFunction(input){
-//     return execute(input)
-// } 
+function execute(input) {
+ console.log(input.elements[0].value);
+ let results = trainedNet(encode(input.elements[0].value));
+ console.log(results);
+ let output;
+ let percent;
+ if (results.trump > results.kardashian) {
+   output = "Donald Trump";
+   percent = Math.floor(results.trump * 100);
+ } else {
+   output = "Kim Kardashian";
+   percent = Math.floor(results.kardashian * 100);
+ }
 
+ alert(`We're ${percent}% certain that written by ${output}`);
+}
 
-console.log(execute("These aren't real. Kanye would never write Yeezy on the side"));
-
-console.log(execute("lock her up! Best ever"))
+train(trainingData);
+console.log(execute("Paste your tweet here"));
